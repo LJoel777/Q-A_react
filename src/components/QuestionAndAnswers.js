8802cf5b3e3004fb4af41539ff29757006914d19
LJoel777 <szaknoel@gmail.com>
2020-08-26 16:02:50 +0200

    Basic structure

---
+++ src/components/QuestionAndAnswers.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuestionAndAnswers = (props) => {
+  return <div></div>;
+};
+
+export default QuestionAndAnswers;
---
+++ src/components/QuestionsList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuestionsList = (props) => {
+  return <div></div>;
+};
+
+export default QuestionsList;
